d1bc5c5f6867bec000d167f88a2fb8f3689e5ced
sakshikakde <kakdesakshi@gmail.com>
2021-10-01 04:25:31 -0400

    Added tests

---
+++ test/test.cpp
@@ -0,0 +1,94 @@
+/**
+ * @file unittest_PIDControllerParam.cpp
+ * @author Sakshi Kakde, Hrushikesh Badhale
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include <pid_controller.hpp>
+/**
+ * @brief Unit test for defaut parameters
+ * 
+ */
+TEST(PIDControllerTest, testPIDControllerDefaltParam) {
+  PIDController *pid = new PIDController();
+  const vector<double> expected_value = {1.0, 1.0, 1.0};
+  // EXPECT_EQ(pid->getGains(), expected_value);
+  EXPECT_EQ(1, 0);
+  delete pid;
+}
+/**
+ * @brief Unit test for user defined parameters
+ * 
+ */
+TEST(PIDControllerTest, testPIDControllerCustomParam) {
+  PIDController *pid = new PIDController();
+  const double Kp = 2.0;
+  const double Ki = 2.0;
+  const double Kd = 2.0;
+  const vector<double> expected_value = {Kp, Ki, Kd};
+  pid->setGains(Kp, Ki, Kd);
+  EXPECT_EQ(pid->getGains(), expected_value);
+  delete pid;
+}
+/**
+ * @brief test Proportional component
+ * 
+ */
+TEST(PIDControllerTest, testPIDControllerP) {
+  PIDController *pid = new PIDController();
+  const double Kp = 0.5;
+  const double Ki = 0.0;
+  const double Kd = 0.0;
+  const double ref_vel = 10.0;
+  const double actual_vel = 5.0;
+  const double expected_output = 2.5;
+  pid->setGains(Kp, Ki, Kd);
+  double computed_output = pid->computeOutput(ref_vel, actual_vel);
+  EXPECT_EQ(computed_output, expected_output);
+  delete pid;
+}
+/**
+ * @brief test Proportional and Derivative component
+ * 
+ */
+TEST(PIDControllerTest, testPIDControllerPD) {
+  PIDController *pid = new PIDController();
+  const double Kp = 1.0;
+  const double Ki = 0.0;
+  const double Kd = 0.5;
+  const double ref_vel = 12.0;
+  const double actual_vel = 5.0;
+  const double expected_output = 6.5;  // Expected output after two iterations
+  pid->setGains(Kp, Ki, Kd);
+  double computed_output = pid->computeOutput(ref_vel, actual_vel);  //  output after 1 iter = 7
+  computed_output = pid->computeOutput(ref_vel, computed_output);  // output after 2 iter = 6.5
+  EXPECT_EQ(computed_output, expected_output);
+  delete pid;
+}
+/**
+ * @brief test Proportional and Integral component
+ * 
+ */
+TEST(PIDControllerTest, testPIDControllerPI) {
+  PIDController *pid = new PIDController();
+  const double Kp = 1.0;
+  const double Ki = 0.5;
+  const double Kd = 0.0;
+  const double ref_vel = 12.0;
+  const double actual_vel = 5.0;
+  const double expected_output = 4.3;  // Expected output after two iterations
+  pid->setGains(Kp, Ki, Kd);
+  double computed_output = pid->computeOutput(ref_vel, actual_vel);  //  e0 = 7, output after 1 iter = 7
+  computed_output = pid->computeOutput(ref_vel, computed_output);  // e1 = 5,
+  // output after 2 iter = (1 * 7) + (0.5 * 6) = 10
+  computed_output = pid->computeOutput(ref_vel, computed_output);  // e2 = 2,
+  // output after 3 iter = (1 * 2) + (0.5 * (7 + 6 + 2) /3) = 4.33
+  EXPECT_NEAR(computed_output, expected_output, 0.1);
+  delete pid;
+}

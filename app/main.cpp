63e1e976a7aec3e4f2dcb5013c79dbcc6c65ee90
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-02 18:33:47 -0400

    Update the main file implementing PID class and invoking it's functions

--- app/main.cpp
+++ app/main.cpp
@@ -8,8 +8,23 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include<iostream>
+#include<vector>
+#include <memory>
 #include <pid_controller.hpp>
 
 int main() {
+    std::unique_ptr<PIDController> pid(new PIDController());
+    const double kp = 0.4;
+    const double ki = 0.01;
+    const double kd = 0.2;
+    const double ref_vel = 20.0;
+    const double actual_vel = 15.0;
+    pid->setGains(kp, ki, kd);
+    pid->setWindowSize(5);
+    double computed_output;
+    for (int i = 0; i< 15; i++)
+      computed_output = pid->computeOutput(ref_vel, computed_output);
+    std::cout << "Coumputed output is : " << computed_output << std::endl;
     return 0;
 }

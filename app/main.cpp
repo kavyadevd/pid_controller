7cfbb3ffa992d00c3da92c942f3c74e1d9c4c45a
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-03 11:35:37 -0400

    Remove unnecessary code from main file

--- app/main.cpp
+++ app/main.cpp
@@ -8,22 +8,8 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include<iostream>
-#include <memory>
 #include <pid_controller.hpp>
 
 int main() {
-    std::unique_ptr<PIDController> pid(new PIDController());
-    const double kp = 0.4;
-    const double ki = 0.01;
-    const double kd = 0.2;
-    const double ref_vel = 20.0;
-    const double actual_vel = 15.0;
-    pid->setGains(kp, ki, kd);
-    pid->setWindowSize(5);
-    double computed_output;
-    for (int i = 0; i< 15; i++)     // Call compute function recursively
-      computed_output = pid->computeOutput(ref_vel, computed_output);
-    std::cout << "Computed output is : " << computed_output << std::endl;
-    return 0;
+  return 0;
 }
